82dab6cf5a83a95d8fc4d82043bb6616e25d9b7d
Arthur Gabriel <arthurelgg@gmail.com>
2022-08-05 15:43:33 -0300

    Add about view

--- src/main/java/arthur/workshop/Main.java
+++ src/main/java/arthur/workshop/Main.java
@@ -1,6 +1,5 @@
-package arthur.workshopjdbcjavafx;
+package arthur.workshop;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
@@ -8,19 +7,25 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends javafx.application.Application {
+
+    private static Scene mainScene;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
         ScrollPane scrollPane = fxmlLoader.load();
 
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
 
-        Scene scene = new Scene(scrollPane);
+        mainScene = new Scene(scrollPane);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(mainScene);
         stage.show();
+    }
+
+    public static Scene getMainScene() {
+        return mainScene;
     }
 
     public static void main(String[] args) {
