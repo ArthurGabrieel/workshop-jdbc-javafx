de325ee51a8a4ceaa77cadd980bce9d46e7a27c4
Arthur Gabriel <arthurelgg@gmail.com>
2022-08-06 23:43:36 -0300

    DepartmentForm controller

---
+++ src/main/java/arthur/workshop/DepartmentFormController.java
@@ -0,0 +1,51 @@
+package arthur.workshop;
+
+import arthur.workshop.util.Constraints;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentFormController implements Initializable {
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private Label labelError;
+
+    @FXML
+    private Button btSave;
+
+    @FXML
+    private Button btCancel;
+
+    @FXML
+    public void onBtSaveAction(ActionEvent event) {
+        System.out.println("Save");
+    }
+
+    @FXML
+    public void onBtCancelAction(ActionEvent event) {
+        System.out.println("Cancel");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        initializeNodes();
+    }
+
+    private void initializeNodes() {
+        Constraints.setTextFieldInteger(txtId);
+        Constraints.setTextFieldMaxLength(txtName, 20);
+    }
+}
+
